2ea87b361e7365e9c04b576ab05a49c7e68d95f5
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-25 13:58:30 +0300

    formating the code for class itme

--- Items.cs
+++ Items.cs
@@ -1,25 +1,38 @@
-namespace ClassItems
+namespace ClassItem
 {
-    class Items
-{
-    public string Name { get; }
-    private int quantity;
-    public int Quantity 
-    { 
-        get { return quantity; } 
-        set { quantity = value; }
-    }
-    private DateTime Date;
-
-    public Items(string name, int quantity, DateTime date = default ) {
-        if (quantity < 0)
+    class Item
+    {
+        public string Name { get; }
+        private int quantity;
+        public int Quantity
         {
-            Console.WriteLine($"Quantity can not be negative");
+            get { return quantity; }
+            set { quantity = value; }
         }
-        Name = name;
-        Quantity = quantity;
-        Date = date == default ? DateTime.Now : date;
-    }
+        private DateTime Date;
 
-}
+        public Item(string name, int quantity, DateTime date = default)
+        {
+            if (quantity < 0)
+            {
+                Console.WriteLine($"Quantity can not be negative");
+            }
+            Name = name;
+            Quantity = quantity;
+            Date = date == default ? DateTime.Now : date;
+        }
+
+        public override string ToString()
+        {
+            return $"Items Name: {Name} Quantity: {Quantity} Date: {Date}";
+        }
+
+        public static void Main(string[] args)
+        {
+            var waterBottle = new Item("Water Bottle", 10, new DateTime(2023, 1, 1));
+
+            Console.WriteLine(waterBottle.ToString());
+
+        }
+    }
 }
---
+++ Store.cs
@@ -0,0 +1,7 @@
+namespace ClassStore
+{
+    class Store
+    {
+        
+    }
+}
