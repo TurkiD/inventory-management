23f4b1d229e00d7bc30aba24e11291adf6b7a768
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-25 15:02:50 +0300

    created addItmesToList method

--- Store.cs
+++ Store.cs
@@ -2,6 +2,33 @@ namespace ClassStore
 {
     class Store
     {
-        
+        private static List<ClassItem.Item> items = new List<ClassItem.Item> { };
+
+        // Here start methods
+        // add/delete items to/from the collection
+        public void AddItemToList(ClassItem.Item item)
+        {
+            try
+            {
+                if (item == null) { throw new ArgumentNullException(); }
+
+                if (items.Contains(item)) { throw new ArgumentException(); }
+
+                items.Add(item);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine($"ERROR: Item Can Not Be Null");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"ERROR: Item Already Exist");
+            }
+        }
+
+        public void DeleteItme(ClassItem.Item item)
+        {
+
+        }
     }
 }
